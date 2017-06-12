73ed9fe2e8f068da2a5d480c5abd040e4bbef540
hardy88 <hardy@hardydemacbook-pro.local>
2017-06-12 15:01:26 +0800

    上传指纹识别代码

---
+++ iOS学习/指纹识别/指纹识别/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  指纹识别
+//
+//  Created by hardy on 2017/6/12.
+//  Copyright © 2017年 胡海涛. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iOS学习/指纹识别/指纹识别/ViewController.m
@@ -0,0 +1,63 @@
+//
+//  ViewController.m
+//  指纹识别
+//
+//  Created by hardy on 2017/6/12.
+//  Copyright © 2017年 胡海涛. All rights reserved.
+//
+
+#import "ViewController.h"
+#import <LocalAuthentication/LocalAuthentication.h>
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    [self test];
+}
+
+
+- (void)test
+{
+    LAContext *context = [[LAContext alloc] init];
+    NSError *error = nil;
+    NSString *reason = @"我们需要验证您的指纹来确认您的身份";
+    // 判断设置是否支持指纹识别(iPhone5s+、iOS8+支持)
+    if([context canEvaluatePolicy:LAPolicyDeviceOwnerAuthenticationWithBiometrics error:&error])
+    {
+        // 指纹识别只判断当前用户是否是机主
+        [context evaluatePolicy:LAPolicyDeviceOwnerAuthenticationWithBiometrics localizedReason:reason reply:^(BOOL success, NSError * _Nullable error) {
+            if(success)
+            {
+                NSLog(@"指纹认证成功");
+            }
+            else
+            {
+                NSLog(@"指纹认证失败");
+                NSLog(@"错误码：%zd",error.code);
+                NSLog(@"出错信息：%@",error);
+                // 错误码 error.code
+                // -1: 连续三次指纹识别错误
+                // -2: 在TouchID对话框中点击了取消按钮
+                // -3: 在TouchID对话框中点击了输入密码按钮
+                // -4: TouchID对话框被系统取消，例如按下Home或者电源键
+                // -8: 连续五次指纹识别错误，TouchID功能被锁定，下一次需要输入系统密码
+            }
+        }];
+    }
+    else
+    {
+        NSLog(@"TouchID设备不可用");
+        NSLog(@"错误码：%zd",error.code);
+        NSLog(@"出错信息：%@",error);
+    }
+}
+
+
+@end
