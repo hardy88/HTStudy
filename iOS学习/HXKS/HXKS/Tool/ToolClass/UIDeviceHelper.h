55c358eff4d48b2ca495c8e811b6c63e5f69dc19
hardy88 <hardy@hardydeMacBook-Pro.local>
2017-10-25 10:31:17 +0800

    提交iPhone型号识别的代码

---
+++ iOS学习/HXKS/HXKS/Tool/ToolClass/UIDeviceHelper.h
@@ -0,0 +1,15 @@
+//
+//  UIDeviceHelper.h
+//  HXKS
+//
+//  Created by hardy on 2017/10/25.
+//  Copyright © 2017年 胡海涛. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface UIDeviceHelper : NSObject
+
++ (NSString *)getDeviceModel;
+
+@end
